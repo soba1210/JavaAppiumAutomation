fc8105b1cfc335cad206837bbc3aaac4d090b875
siinc <soba1210@gmail.com>
2019-07-25 18:07:01 +0300

    домашка

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,7 +3,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -12,30 +16,75 @@ public class FirstTest {
     private AppiumDriver driver;
 
     @Before
-    public void setUp() throws Exception
-    {
+    public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("deviceName","AndroidTestDevice");
-        capabilities.setCapability("platformVersion","9");
-        capabilities.setCapability("automationName","Appium");
-        capabilities.setCapability("appPackage","org.wikipedia");
-        capabilities.setCapability("appActivity",".main.MainActivity");
-        capabilities.setCapability("app","/Users/siinc/Desktop/JavaAppiumAutomation/JavaAppiumAutomation/apk/org.wikipedia.apk");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "9");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "/Users/siinc/Desktop/JavaAppiumAutomation/JavaAppiumAutomation/apk/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
     }
 
     @After
-    public void tearDown()
-    {
+    public void tearDown() {
+
         driver.quit();
     }
+
     @Test
-    public void firstTest()
-    {
-        System.out.println("First test run");
+    public void firstTest() throws InterruptedException {
+
+        waitForElementByXpathAndClick(
+                "//*[contains(@text,'Search Wikipedia')]",
+                "Cannot find Search Wikipedia input",
+                5
+        );
+
+        waitForElementByXpathAndSendKeys(
+                "//*[contains(@text,'Search…')]",
+                "keyword not found",
+                "Java",
+                5
+        );
+
+
+        waitForElementPresentByXpath
+                (
+                        "//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Java (programming language)']",
+                        "Cannot find 'Java (programming language)' topic searching by Java",
+                        12
+                );
+
+
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String error_message) {
+        return waitForElementPresentByXpath(xpath, error_message, 5);
+    }
+
+    private WebElement waitForElementByXpathAndClick(String xpath, String error_messages, long timeInSeconds) {
+        WebElement element = waitForElementPresentByXpath(xpath, error_messages, timeInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementByXpathAndSendKeys(String xpath, String error_messages, String value, long timeInSeconds) {
+        WebElement element = waitForElementPresentByXpath(xpath, error_messages, timeInSeconds);
+        element.sendKeys(value);
+        return element;
     }
 }
+
